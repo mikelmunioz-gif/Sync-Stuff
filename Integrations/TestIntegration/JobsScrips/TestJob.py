4c934c574a5d30a4d7eb74dea8942dac65e811ec
Mikel Muñoz <mikel.munioz@pers.eus>
2025-09-08 10:25:17 +0000

    Cambio realizado 4

--- Integrations/TestIntegration/JobsScrips/TestJob.py
+++ Integrations/TestIntegration/JobsScrips/TestJob.py
@@ -15,7 +15,7 @@ def main():
 
     try:
         siemplify.LOGGER.info("🚀 TestJob - job de prueba")
-        siemplify.LOGGER.info("🚀 Mal 3")
+        siemplify.LOGGER.info("🚀 Mal 4")
         # Aquí irá tu lógica real cuando quieras
     except Exception as e:
         status = EXECUTION_STATE_FAILED
