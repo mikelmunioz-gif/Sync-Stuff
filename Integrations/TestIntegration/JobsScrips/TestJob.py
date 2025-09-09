7f4de59fba3a1c06afb3eff280ff6c8a250f12b0
Mikel Muñoz <mikel.munioz@pers.eus>
2025-09-09 10:33:36 +0000

    Todo a 10

--- Integrations/TestIntegration/JobsScrips/TestJob.py
+++ Integrations/TestIntegration/JobsScrips/TestJob.py
@@ -15,7 +15,7 @@ def main():
 
     try:
         siemplify.LOGGER.info("🚀 TestJob - job de prueba")
-        siemplify.LOGGER.info("🚀 ESTO NO DEBERÍA VERSE 9")
+        siemplify.LOGGER.info("🚀 ESTO NO DEBERÍA VERSE 10")
         # Aquí irá tu lógica real cuando quieras
     except Exception as e:
         status = EXECUTION_STATE_FAILED
