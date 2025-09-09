2c5cb34cd083c167bb5b8be28f7146d0182891b1
Mikel Muñoz <mikel.munioz@pers.eus>
2025-09-09 07:06:07 +0000

    Mover un connector 3

--- Integrations/TestIntegration/ConnectorsScripts/MySecondTestConnector.py
+++ Integrations/TestIntegration/ConnectorsScripts/MySecondTestConnector.py
@@ -29,7 +29,7 @@ def main():
 
     siemplify.LOGGER.info("===== Iniciando conector de ejemplo =====")
     print("Hola mundo")   # salida estándar
-    siemplify.LOGGER.info("MySecondTestConnector 2")  # salida en logs de SecOpsproi
+    siemplify.LOGGER.info("MySecondTestConnector 3")  # salida en logs de SecOpsproi
 
     # Siempre hay que devolver una lista de casos (aunque vacía)
     return []
