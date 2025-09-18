2cded5b01d461a5ad496510c787005a2cf76b489
Mikel Muñoz <mikel.munioz@pers.eus>
2025-09-18 12:17:26 +0200

    Nueva integración

---
+++ Integrations/TestIntegration2/JobsScrips/HelloJob.py
@@ -0,0 +1,30 @@
+from SiemplifyJob import SiemplifyJob
+from SiemplifyUtils import output_handler
+from ScriptResult import EXECUTION_STATE_COMPLETED, EXECUTION_STATE_FAILED
+
+SCRIPT_NAME = "HelloJob"
+
+@output_handler
+def main():
+    siemplify = SiemplifyJob()
+    siemplify.script_name = SCRIPT_NAME
+
+    # Lee el parámetro que definimos en el .jobdef
+    TEST = siemplify.extract_job_param("Test", input_type=str, is_mandatory=True)
+
+    status = EXECUTION_STATE_COMPLETED
+    result_value = True
+    output_message = "OK"
+
+    try:
+        siemplify.LOGGER.info(f"🚀 HelloJob(TestIntegration2) | Test={TEST}")
+        # tu lógica real aquí
+    except Exception as e:
+        status = EXECUTION_STATE_FAILED
+        result_value = False
+        output_message = f"Error: {e}"
+
+    siemplify.end(output_message, result_value, status)
+
+if __name__ == "__main__":
+    main()
