b7989269df94c7b5bceef23f04834a6cc2608ba8
Mikel Muñoz <mikel.munioz@pers.eus>
2025-10-06 15:50:29 +0200

    Mod HelloJob2

--- Integrations/TestIntegration2/JobsScrips/HelloJob2.py
+++ Integrations/TestIntegration2/JobsScrips/HelloJob2.py
@@ -18,7 +18,7 @@ def main():
 
     try:
         siemplify.LOGGER.info(f"🚀 HelloJob(TestIntegration2) | Test={TEST}")
-        siemplify.LOGGER.info(f"Mensaje de prueba. Si sale es que todo ha salido bien. CAMBIO HECHO 1")
+        siemplify.LOGGER.info(f"Mensaje de prueba. Si sale es que todo ha salido bien. CAMBIO HECHO 1. SI VES ESTO AHORA, ENTONCES ES QUE TODO BIEN")
         # tu lógica real aquí
     except Exception as e:
         status = EXECUTION_STATE_FAILED
